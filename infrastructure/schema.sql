67ae7816d910a28cd7e3ab9491cea8aeeb5f8e2b
Barury797 <158559616+Barury797@users.noreply.github.com>
2024-04-30 23:10:09 +0600

    Added problem_sets and problem tables

--- infrastructure/schema.sql
+++ infrastructure/schema.sql
@@ -4,7 +4,30 @@ USE sql_db;
 
 DROP TABLE IF EXISTS `lectures`;
 CREATE TABLE lectures (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    lecture_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
-    body TEXT NOT NULL
+    body TEXT NOT NULL,
+    category ENUM('math', 'technology', 'chemistry', 'physics', 'english_language', 'french_language', 'history') NOT NULL,
+    created DATETIME DEFAULT CURRENT_TIMESTAMP,
+    modified DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS `problemsets`;
+CREATE TABLE problemsets (
+    problemset_id INT PRIMARY KEY AUTO_INCREMENT,
+    lecture_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    body TEXT NOT NULL,
+    created DATETIME DEFAULT CURRENT_TIMESTAMP,
+    modified DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS `problems`;
+CREATE TABLE problems (
+    problem_id INT PRIMARY KEY AUTO_INCREMENT,
+    problemset_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    body TEXT NOT NULL,
+    created DATETIME DEFAULT CURRENT_TIMESTAMP,
+    modified DATETIME DEFAULT CURRENT_TIMESTAMP
 );
