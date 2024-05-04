8674ae9fa2a6e5d76fe152f85c89cc4b1a3a174f
ducky-dev11 <bob0010thebuilder@gmail.com>
2024-05-04 18:46:22 +0530

    app.jsx change

--- frontend/lecture-app/src/App.jsx
+++ frontend/lecture-app/src/App.jsx
@@ -1,22 +1,28 @@
 import './App.css'
-import { BrowserRouter } from 'react-router-dom';
+// import { Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
 import Header from './components/Home/Header'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
-  return (<>
 
-    <BrowserRouter>
-    <Navbar />
-    <br />
-    <Header />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-    </BrowserRouter>
-
-  </>)
+return(
+  <>
+  <Navbar/>
+  <Header/>
+  </>
+)
+//   return <Router>
+//     <Navbar />
+//     <Header />
+// {/*     
+//   <Switch>
+//     <Route exact path="/" component={Home} />
+//     <Route path="/About/About" component={About} />
+//   </Switch>
+//   </Router> */}
 }
 
 export default App
--- frontend/lecture-app/src/components/Contact/Contact.jsx
+++ frontend/lecture-app/src/components/Contact/Contact.jsx
@@ -4,6 +4,9 @@ const Contact = () => {
     return (
         <>
         
+
+
+
         </>
     )
 }
