2fc30cb1edc915f11fc6b365d98bac250eca916c
Ben Evanoff <evanoff3@illinois.edu>
2024-05-20 17:38:05 -0500

    login error for invalid creds

--- frontend/src/components/Signin/Signin.jsx
+++ frontend/src/components/Signin/Signin.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState } from 'react';
+import "./Signin.css"
 
 const HOST =  "http://localhost:8080/login"; // TODO: replace with environment variable
 
@@ -7,6 +8,7 @@ const SignIn = () => {
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [errorMsg, setError] = useState(false);
 
     const submitSignIn = async () => {
         try {
@@ -22,20 +24,25 @@ const SignIn = () => {
             );
       
             if (response.status === 200) {
+                setError(false);
                 console.log("loggin success");
             }
           } catch (error) {
+            if (error.response.status == 403) setError('Invalid Credentials');
             console.log(error.message);
           }
     };
 
     return (<>
+        <center>
+        {errorMsg && <p>Error: {errorMsg}</p>}
         <h1>Sign In</h1>
-        <div>
+        <div className="input-boxes">
             <input type="text" id="signin-username-input" onChange={ (e) => setUsername(e.target.value)}/>
             <input type="password" id="signin-password-input" onChange={(e) => setPassword(e.target.value)}/>
         </div>
         <button onClick={submitSignIn}>Submit</button>
+        </center>
     </>);
 };
 
