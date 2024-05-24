5528f786058b5cfc462ef50bb622c703dc83acef
ducky-dev11 <bob0010thebuilder@gmail.com>
2024-05-24 11:51:15 +0530

    link color change if on the referenced page

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,19 @@
-
-import styles from"./Navbar.module.css"
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from "react-router-dom";
-import { HiAcademicCap } from "react-icons/hi2";
+import styles from "./Navbar.module.css";
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useLocation } from "react-router-dom";
+import { HiAcademicCap } from "react-icons/hi";
 import { Link } from 'react-router-dom';
-
 
 const Navbar = (props) => {
     const nav = useNavigate();
-    const options_logged_out = ["Home", "Sign In", "About"]
-    const options_logged_in = ["Home", "Profile", "About"]
-    const links = {"Home": "/", "Sign In": "/signin", "About": "/about", "Contact": "contact", "Profile": "/profile"};
+    const location = useLocation();
+    const options_logged_out = ["Home", "Sign In", "About"];
+    const options_logged_in = ["Home", "Profile", "About"];
+    const links = {"Home": "/", "Sign In": "/signin", "About": "/about", "Contact": "/contact", "Profile": "/profile"};
+
     return (
         <>
-            <nav className= {styles.container}>
+            <nav className={styles.container}>
                 <div className={styles.logo}>
                     <HiAcademicCap size="40px" />
                     <h1>LecQuiz</h1>
@@ -21,16 +21,24 @@ const Navbar = (props) => {
                 <div className={styles.list_container}>
                     <ul className={styles.ul_options}>
                         {props.isLoggedIn && options_logged_in.map((option) => {
-                            return <li key={option} onClick={() => {nav(links[option])}}>{option}</li>
+                            return (
+                                <li key={option} onClick={() => {nav(links[option])}} style={{ color: location.pathname === links[option] ? "black" : "inherit" }}>
+                                    {option}
+                                </li>
+                            );
                         })}
                         {!props.isLoggedIn && options_logged_out.map((option) => {
-                            return <li key={option} onClick={() => {nav(links[option])}}>{option}</li>
+                            return (
+                                <li key={option} onClick={() => {nav(links[option])}} style={{ color: location.pathname === links[option] ? "black" : "inherit" }}>
+                                    {option}
+                                </li>
+                            );
                         })}
                     </ul>
                 </div>
             </nav>
         </>
-    )
+    );
 }
 
 export default Navbar;
