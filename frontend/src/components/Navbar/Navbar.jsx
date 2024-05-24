d9bfd159ce63daf6f9c99a9f4437a22551976d75
ducky-dev11 <bob0010thebuilder@gmail.com>
2024-05-24 11:35:01 +0530

    update from main to frontend-track

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-import React from "react";
-import styles from "./Navbar.module.css"
-=======
+
 import styles from"./Navbar.module.css"
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from "react-router-dom";
->>>>>>> origin/main
 import { HiAcademicCap } from "react-icons/hi2";
 import { Link } from 'react-router-dom';
 
@@ -15,30 +11,6 @@ const Navbar = (props) => {
     const options_logged_out = ["Home", "Sign In", "About"]
     const options_logged_in = ["Home", "Profile", "About"]
     const links = {"Home": "/", "Sign In": "/signin", "About": "/about", "Contact": "contact", "Profile": "/profile"};
-
-<<<<<<< HEAD
-    return (<>
-        <nav className={styles.container}>
-            <div className={styles.logo}>
-                <HiAcademicCap size="40px" />
-                <h1>LecQuiz</h1>
-            </div>
-            <div className={styles.list_container}>
-                <ul className={styles.ul_options}>
-                    {options.map((option) => (
-                        <Link key={option} to={`/${option.toLowerCase()}`}>{option}</Link>
-                    ))}
-                </ul>
-            </div>
-        </nav>
-    </>)
-}
-
-function myFunction(option) {
-    if (option === "Home") {
-        console.log("directed to Home");
-    }
-=======
     return (
         <>
             <nav className= {styles.container}>
@@ -59,7 +31,6 @@ function myFunction(option) {
             </nav>
         </>
     )
->>>>>>> origin/main
 }
 
 export default Navbar;
